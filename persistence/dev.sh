31bebf1561bffab2b15935ffbc8af7e8ff0f246e
Pablo Mansilla Ojeda <pablo@ppamo.cl>
2022-05-26 11:07:19 -0400

    Fix user creation in init script

--- persistence/dev.sh
+++ persistence/dev.sh
@@ -5,12 +5,13 @@ NAME=mongo_dev
 
 docker run -ti --rm \
 	--name $NAME \
+	--net host \
 	--env MONGO_INITDB_ROOT_USERNAME=admin \
 	--env MONGO_INITDB_ROOT_PASSWORD=123456 \
 	--env DB_USERNAME=app \
 	--env DB_PASSWORD=654321 \
 	--env AUTHDB=admin \
-	--env DB=products \
+	--env DB_NAME=products \
 	--env COLLECTION=docs \
 	--publish 27017:27017 \
 	--volume $PWD/data:/data\
--- persistence/init/init.sh
+++ persistence/init/init.sh
@@ -5,17 +5,17 @@
 printf -- "- Creating user:\n"
 mongo --username $MONGO_INITDB_ROOT_USERNAME \
 	--password $MONGO_INITDB_ROOT_PASSWORD \
-	--authenticationDatabase $AUTHDB << EOF
-db.createUser({ user: "$DB_USERNAME", pwd: "$DB_PASSWORD", roles: [ { role: 'readWrite', db: "$DB" } ]})
+	--authenticationDatabase $AUTHDB $DB_NAME << EOF
+db.createUser({ user: "$DB_USERNAME", pwd: "$DB_PASSWORD", roles: [ 'readWrite' ]})
 EOF
 
 # Import data
-printf -- "- Creating user:\n"
+printf -- "- Importing data:\n"
 mongoimport \
 	--username $MONGO_INITDB_ROOT_USERNAME \
 	--password $MONGO_INITDB_ROOT_PASSWORD \
 	--authenticationDatabase $AUTHDB \
-	--db $DB \
+	--db $DB_NAME \
 	--collection $COLLECTION \
 	--mode upsert \
 	--file /docker-entrypoint-initdb.d/01-products.json
